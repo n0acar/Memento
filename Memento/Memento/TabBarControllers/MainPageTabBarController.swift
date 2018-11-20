5a922b3a6dd094adb2dc3e9b48180ba07e4a795a
Nevzat Acar <nacar14@ku.edu.tr>
2018-11-20 19:14:30 +0300

    Tab Bar Controller is added.

---
+++ Memento/Memento/TabBarControllers/MainPageTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainPageTabBarController.swift
+//  Memento
+//
+//  Created by Nevzat Acar on 20.11.2018.
+//  Copyright © 2018 Brothers. All rights reserved.
+//
+
+import UIKit
+
+class MainPageTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 2
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
