bc5e2f599b8f7ecedc09f6cc828b9b978f767e48
Nevzat Acar <nacar14@ku.edu.tr>
2018-11-20 17:32:59 +0300

    Project Creation

---
+++ Memento/Memento/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memento
+//
+//  Created by Nevzat Acar on 20.11.2018.
+//  Copyright © 2018 Brothers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
